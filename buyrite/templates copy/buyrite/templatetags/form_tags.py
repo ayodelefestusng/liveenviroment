0859c0edf3416070183dcf679865d288a395ce49
ayodelefestusng <ayodelefestusng@gmail.com>
2025-09-19 08:19:37 +0100

    I need to amend become a dealer and amend the navbar

--- buyrite/templates copy/buyrite/templatetags/form_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def get_field(form, field_name):
-    """Get form field by name"""
-    return form[field_name]
---
+++ users/templatetags/custom_filters.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def first_name(full_name):
+    return full_name.split()[0] if full_name else ''
