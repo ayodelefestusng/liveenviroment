b2d01bd8bb8b488a91b26d94952a2ee2312b27f1
Ayodele Adeyinka <ayodelefestusng@gmail.com>
2025-09-28 11:57:52 +0100

    Updated settings and URLs

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,19 +1,10 @@
 """
 URL configuration for myproject project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information, see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -21,18 +12,20 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("users.urls")),
-    path("payments/", include("crossborder.urls")),
-     path("ai/", include("ai.urls")),
-     path("buyrite/", include("buyrite.urls")),
-     path("laundry/", include("laundry.urls")),
-    path("store/", include("store.urls")),
-   
-   
-    path('accounts/', include('django.contrib.auth.urls')),  # 👈 This line is key
 
+    # Core apps
+    path('', include(('users.urls', 'users'), namespace='users')),
+    path('payments/', include(('crossborder.urls', 'crossborder'), namespace='crossborder')),
+    path('ai/', include(('ai.urls', 'ai'), namespace='ai')),
+    path('buyrite/', include(('buyrite.urls', 'buyrite'), namespace='buyrite')),
+    path('laundry/', include(('laundry.urls', 'laundry'), namespace='laundry')),
+    path('store/', include(('store.urls', 'store'), namespace='store')),
+
+    # Authentication
+    path('accounts/', include('django.contrib.auth.urls')),  # Built-in login/logout/password views
+    path('accountss/', include('allauth.urls')),              # Social login (Google, Facebook, etc.)
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
